029b0b07a515c40f33688c2ddc00138b9ecd287b
Ubuntu <ubuntu@ip-172-31-21-255.ap-southeast-1.compute.internal>
2015-04-07 09:14:46 +0000

    Before Bower

--- public/src/js/controllers/ListCtrl.js
+++ public/src/js/controllers/ListCtrl.js
@@ -1,4 +1,10 @@
 angular.module('MainCtrl')
     .controller('listController', ['$scope', 'api', function($scope, api) {
-        $scope.message = "List";
+        api.query({'set':'raid'}, function(data) {
+            for(var x in data) {
+                //data[x].time_created = moment(data[x].time_created).tz($scope.timezone);
+                //data[x].play_time = moment(data[x].time_created).tz($scope.timezone);
+            }
+            $scope.raids = data;
+        });
     }]);
--- public/src/js/rms.js
+++ public/src/js/rms.js
@@ -1,2 +1,6 @@
 angular.module('rmsApp', ['MainCtrl', 'TestService']);
-angular.module('MainCtrl', ['ngResource']);
+angular.module('MainCtrl', ['ngResource', 'angularMoment']);
+angular.module('MainCtrl').value('angularMomentConfig', {
+    preprocess: 'utc',
+    timezone: 'Europe/London'
+});
